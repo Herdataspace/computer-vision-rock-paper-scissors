be4f46337bad16941cf1ebe4894e9e5db780de24
herdataspace <stephaniehunter01@gmail.com>
2023-03-20 11:29:33 +0000

    Adding computer choice and user input functions to manual_rps file

---
+++ manual_rps.py
@@ -0,0 +1,12 @@
+import random
+
+def get_computer_choice():
+    rps_choice = ['Rock', 'Paper', 'Scissors']
+    computer_choice = random.choice(rps_choice)
+    print(computer_choice)
+
+def get_user_choice():
+    user_choice = input('Rock, Paper, Scissors?')
+
+
+
