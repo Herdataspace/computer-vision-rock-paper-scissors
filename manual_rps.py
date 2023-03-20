f8a158a0d22b0754eac5834ca0418abbc24aefe1
herdataspace <stephaniehunter01@gmail.com>
2023-03-20 12:29:40 +0000

    Removing return statements from the get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -15,21 +15,15 @@ def get_winner(computer_choice, user_choice):
     elif computer_choice == "Rock":
         if user_choice == "Paper":
             print("You won!")
-            return user_choice
         if user_choice == "Scissors":
             print("You lost!")
-            return computer_choice
     elif computer_choice == "Paper":
         if user_choice == "Scissors":
             print("You won!")
-            return user_choice
         if user_choice == "Rock":
             print("You lost!")
-            return computer_choice
     elif computer_choice == "Scissors":
         if user_choice == "Rock":
             print("You won!")
-            return user_choice
         if user_choice == "Paper":
             print("You lost!")
-            return computer_choice
