1527879835044cf063a909dd9575e7d61ff6aecb
herdataspace <stephaniehunter01@gmail.com>
2023-03-20 12:24:03 +0000

    Adding function to work out winner

--- manual_rps.py
+++ manual_rps.py
@@ -1,10 +1,35 @@
 import random
 
 def get_computer_choice():
-    rps_choice = ['Rock', 'Paper', 'Scissors']
-    computer_choice = random.choice(rps_choice)
+    possible_choices = ['Rock', 'Paper', 'Scissors']
+    computer_choice = random.choice(possible_choices)
     return computer_choice
 
 def get_user_choice():
     user_choice = input('Rock, Paper, Scissors? ')
     return user_choice
+
+def get_winner(computer_choice, user_choice):
+    if user_choice == computer_choice:
+        print("It is a tie!" )
+    elif computer_choice == "Rock":
+        if user_choice == "Paper":
+            print("You won!")
+            return user_choice
+        if user_choice == "Scissors":
+            print("You lost!")
+            return computer_choice
+    elif computer_choice == "Paper":
+        if user_choice == "Scissors":
+            print("You won!")
+            return user_choice
+        if user_choice == "Rock":
+            print("You lost!")
+            return computer_choice
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You won!")
+            return user_choice
+        if user_choice == "Paper":
+            print("You lost!")
+            return computer_choice
