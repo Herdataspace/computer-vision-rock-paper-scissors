21d031b46adf8aae36cfd86bf584a6f69c791d58
herdataspace <stephaniehunter01@gmail.com>
2023-03-20 11:43:11 +0000

    Adding return statements to the functions

--- manual_rps.py
+++ manual_rps.py
@@ -3,7 +3,8 @@ import random
 def get_computer_choice():
     rps_choice = ['Rock', 'Paper', 'Scissors']
     computer_choice = random.choice(rps_choice)
-    print(computer_choice)
+    return computer_choice
 
 def get_user_choice():
     user_choice = input('Rock, Paper, Scissors? ')
+    return user_choice
