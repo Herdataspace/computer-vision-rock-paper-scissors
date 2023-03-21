876d8ee521606de8a761011eea450fa599b61af3
herdataspace <stephaniehunter01@gmail.com>
2023-03-21 10:38:44 +0000

    Replacing the hard coded user input with the get_prediciton() function in the game to return the output of the model

--- camera_rps.py
+++ camera_rps.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 from keras.models import load_model
 import numpy as np
 
@@ -29,16 +30,49 @@ def get_prediction():
 
     if user_choice_prediction == 0:
         user_choice = 'Rock'
-        print('user_choice')
+        print(user_choice)
     elif user_choice_prediction == 1:
         user_choice = 'Paper'
-        print('user_choice')
+        print(user_choice)
     elif user_choice_prediction == 2:
         user_choice = 'Scissors'
-        print('user_choice')
+        print(user_choice)
     else:
         user_choice = 'Nothing'
-        print('user_choice')
+        print(user_choice)
+    
     return user_choice
 
-get_prediction()
+def get_computer_choice():
+    possible_choices = ['Rock', 'Paper', 'Scissors']
+    computer_choice = random.choice(possible_choices)
+    print(f'Computer chose: {computer_choice}')
+    return computer_choice
+
+def get_user_choice():
+    user_choice = get_prediction()
+    return user_choice
+
+def get_winner(computer_choice, user_choice):
+    if user_choice == computer_choice:
+        print("It is a tie!" )
+    elif computer_choice == "Rock":
+        if user_choice == "Paper":
+            print("You won!")
+        elif user_choice == "Scissors":
+            print("You lost")
+    elif computer_choice == "Paper":
+        if user_choice == "Scissors":
+            print("You won!")
+        elif user_choice == "Rock":
+            print("You lost")
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You won!")
+        elif user_choice == "Paper":
+            print("You lost")
+
+def play():
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    get_winner(computer_choice, user_choice)
